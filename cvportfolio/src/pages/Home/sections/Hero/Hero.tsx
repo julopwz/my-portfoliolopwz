ab7ea1b474a5de79a1b721242424a3f2591eb96b
Julia Lopes <julialopes@usp.br>
2024-10-21 22:01:01 -0300

    new grid with material ui

--- cvportfolio/src/pages/Home/sections/Hero/Hero.tsx
+++ cvportfolio/src/pages/Home/sections/Hero/Hero.tsx
@@ -1,29 +1,39 @@
-import { Container, styled, Typography } from "@mui/material"
-import Avatar from "../../../../assets/images/avatar.jpg"
-import Grid from '@mui/material/Grid';
+import { Button, Container, styled, Typography } from "@mui/material";
+import Avatar from "../../../../assets/images/avatar.jpg";
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import EmailIcon from '@mui/icons-material/Email';
+import Grid from '@mui/material/Grid2';
+
 
 const Hero = () => {
 
     const StyledHero = styled("div")(() => ({
-        backgroundColor: "black"
+        backgroundColor: "black",
+        height: "100vh"
     }))
 
     const StyledImg = styled("img")(() => ({
-        width: "30%",
+        width: "100%",
         borderRadius: "50%"
     }))
 
-
     return (
         <StyledHero>
-
             <Container>
                 <Grid container spacing={2}>
-                    <Grid item xs={4} >
-                        <StyledImg src={Avatar} />
+                    <Grid size={{ xs: 12, md: 4 }} >
+                    <StyledImg src={Avatar} />
                     </Grid>
-                    <Grid item xs={8} >
-                    <Typography color="primary">Júlia Lopes Carvalho</Typography>
+                    <Grid size={{ xs: 12, md: 8 }}>
+            
+                    <Typography color="primary" variant="h1" textAlign="center">Júlia Lopes Carvalho</Typography>
+                        <Typography color="primary" variant="h2" textAlign="center">I'm a Librarian | Information Specialist | Front-end Developer</Typography>
+                        <Button>
+                            <FileDownloadIcon />
+                            Download CV</Button>
+                        <Button>
+                            <EmailIcon />
+                            Contact me</Button>
                     </Grid>
                 </Grid>
             </Container>
