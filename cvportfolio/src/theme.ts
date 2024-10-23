e44f2d2ea6e75e4e4a850831c3f026cb935c0eea
Julia Lopes <julialopes@usp.br>
2024-10-22 21:59:39 -0300

    new style navbar and background

--- cvportfolio/src/theme.ts
+++ cvportfolio/src/theme.ts
@@ -2,29 +2,29 @@
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 
 let theme = createTheme({
-    palette: {
-      primary: {
-        main: '#111439',
-      },
-      secondary: {
-        main:  '#96C2DB',
-      },
+  palette: {
+    primary: {
+      main: '#ffffff',
     },
-    typography: {
-        fontFamily: [
-            '"Helvetica Neue"',
-            // '-apple-system',
-            // 'BlinkMacSystemFont',
-            // '"Segoe UI"',
-            // 'Roboto',
-            // '"Helvetica Neue"',
-            // 'Arial',
-            // 'sans-serif',
-            // '"Apple Color Emoji"',
-            // '"Segoe UI Emoji"',
-            // '"Segoe UI Symbol"',
-        ].join(','),
-    }
+    secondary: {
+      main: '#111439',
+    },
+  },
+  typography: {
+    fontFamily: [
+      '"Helvetica Neue"',
+      // '-apple-system',
+      // 'BlinkMacSystemFont',
+      // '"Segoe UI"',
+      // 'Roboto',
+      // '"Helvetica Neue"',
+      // 'Arial',
+      // 'sans-serif',
+      // '"Apple Color Emoji"',
+      // '"Segoe UI Emoji"',
+      // '"Segoe UI Symbol"',
+    ].join(','),
+  }
 });
 
 theme = responsiveFontSizes(theme);
